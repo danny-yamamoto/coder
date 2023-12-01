b7a77d725143a76ce9ba07885912f2c08c20a8b8
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-12-01 10:48:31 +0000

    Refactoring

--- rust_gpt_string_processing_1/src/main.rs
+++ rust_gpt_string_processing_1/src/main.rs
@@ -1,19 +1,9 @@
-fn char_cnt(input: &str, search_text: &str) -> i32 {
-    let mut ret = 0;
-    if input.is_empty() {
-        return ret;
-    }
-    for c in input.chars() {
-        if c.to_string()  == search_text {
-            ret += 1;
-        }
-    }
-    return  ret;
+fn char_cnt(input: &str, search_char: char) -> usize {
+    input.matches(search_char).count()
 }
 
 fn main() {
     let input = "Hello, world!".to_string();
-    let search_text = "l".to_string();
-    let cnt = char_cnt(&input, &search_text);
+    let cnt = char_cnt(&input, 'l');
     println!("{}", cnt);
 }
